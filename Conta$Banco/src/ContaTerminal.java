ba182873d0182c4e2f1476dc8fa72ce8faeb4617
lucas-D-ramos-of <lucasandraderamosof@gmail.com>
2025-03-16 19:04:18 -0300

    Instruções criadas

--- Conta$Banco/src/ContaTerminal.java
+++ Conta$Banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner entrada = new Scanner (System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nomeCliente = entrada.next();
+
+        System.out.println("Digite sua agência: ");
+        String agenciaCliente = entrada.next();
+
+        System.out.println("Digite o número da agência: ");
+        int numeroAgenciaCliente = entrada.nextInt();
+
+        System.out.println("Digite seu saldo: ");
+        Float saldoCliente = entrada.nextFloat();
+
+        System.out.println("");
+        entrada.close();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %-.2f já está disponível para saque", nomeCliente, agenciaCliente, numeroAgenciaCliente, saldoCliente);
     }
 }
